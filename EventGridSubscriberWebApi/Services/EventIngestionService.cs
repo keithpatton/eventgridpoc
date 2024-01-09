5004ec11cdf00cfb129d0b2f60931dcf3dc23167
Keith Patton <Keith.Patton@serko.com>
2024-01-09 14:41:28 +1300

    added a couple of examples around how to use Mediatr for a generic EventIngestionService.

---
+++ EventGridSubscriberWebApi/Services/EventIngestionService.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Messaging;
+using EventIngestionServices.Abstractions;
+using MediatR;
+
+namespace EventGridSubscriberWebApi.Services
+{
+
+    /// <summary>
+    /// Processes all events
+    /// </summary>
+    public class EventIngestionService : IEventIngestionService
+    {
+        private readonly IMediator _mediator;
+
+        public EventIngestionService(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task IngestAsync(CloudEvent cloudEvent)
+        {
+            await _mediator.Send(new IngestEventRequest(cloudEvent));
+        }
+    }
+
+    public class IngestEventRequest : IRequest
+    {
+        public CloudEvent CloudEvent { get; }
+
+        public IngestEventRequest(CloudEvent cloudEvent)
+        {
+            CloudEvent = cloudEvent;
+        }
+    }
+
+}
