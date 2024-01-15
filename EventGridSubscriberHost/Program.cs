1994418b786fa4084faa0a9b2715a188337f499b
Keith Patton <Keith.Patton@serko.com>
2024-01-16 09:01:44 +1300

    added host ingestion

---
+++ EventGridSubscriberHost/Program.cs
@@ -0,0 +1,40 @@
+﻿using EventGridSubscriberWebApi.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serko.Messaging.EventIngestion.Extensions;
+
+namespace EventGridSubscriberHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var builder = Host.CreateDefaultBuilder(args)
+                 .ConfigureAppConfiguration((hostingContext, config) =>
+                 {
+                     var env = hostingContext.HostingEnvironment;
+                     config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                     if (env.IsDevelopment())
+                     {
+                         config.AddUserSecrets<Program>();
+                     }
+                 })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var config = hostContext.Configuration;
+                    services.AddEventGridIngestion<TestEventIngestionService>(
+                        eventsIngestionHostedServiceOptions: opts => config.GetSection("EventsIngestionHostedService").Bind(opts),
+                        eventGridIngestionServiceOptions: opts => config.GetSection("EventGridIngestionService").Bind(opts),
+                        redisLockServiceOptions: opts => config.GetSection("RedisLockService").Bind(opts));
+                });
+            return builder;
+        }
+    }
+}
---
+++ EventGridSubscriberHost/Services/TestEventIngestionService.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Messaging;
+using Microsoft.Extensions.Logging;
+using Serko.Messaging.EventIngestion.Abstractions;
+
+namespace EventGridSubscriberWebApi.Services
+{
+
+    /// <summary>
+    /// Ingests an Event using Sql Database
+    /// </summary>
+    public class TestEventIngestionService : IEventIngestionService
+    {
+        private readonly ILogger<TestEventIngestionService> _logger;
+
+        public TestEventIngestionService(ILogger<TestEventIngestionService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task IngestAsync(CloudEvent cloudEvent)
+        {
+            _logger.LogInformation($"Cloud Event {cloudEvent.Id} received");
+            await Task.CompletedTask;
+        }
+
+       
+    }
+}
