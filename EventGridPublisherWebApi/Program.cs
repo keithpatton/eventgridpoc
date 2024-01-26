49fabec7d721929c099e562b5df29235839399ec
Keith Patton <Keith.Patton@serko.com>
2024-01-26 14:48:42 +1300

    default to memory queue

--- EventGridPublisherWebApi/Program.cs
+++ EventGridPublisherWebApi/Program.cs
@@ -22,10 +22,10 @@ namespace EventGridPublisherWebApi
             // Add event grid publishing 
 
             // SQL QUEUE (QUEUE RESILIENCE)
-            builder.Services.Configure<SqlEventQueueServiceOptions>(builder.Configuration.GetSection("SqlEventQueueService"));
-            builder.Services.AddEventGridPublishing<SqlEventQueueService>(
-                eventGridPublishingServiceOptions: opts => builder.Configuration.GetSection("EventGridPublishingService").Bind(opts)
-            );
+            //builder.Services.Configure<SqlEventQueueServiceOptions>(builder.Configuration.GetSection("SqlEventQueueService"));
+            //builder.Services.AddEventGridPublishing<SqlEventQueueService>(
+            //    eventGridPublishingServiceOptions: opts => builder.Configuration.GetSection("EventGridPublishingService").Bind(opts)
+            //);
 
             // MEMORY QUEUE
             builder.Services.AddEventGridPublishing<MemoryEventQueueService>(
